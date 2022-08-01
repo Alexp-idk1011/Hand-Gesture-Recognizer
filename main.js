af6627f4a3f65965be43130846c1c824a835a20d
Alexp-idk1011 <83146847+Alexp-idk1011@users.noreply.github.com>
2022-07-31 19:17:10 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,26 +1,73 @@
-function preload() {
+Webcam.set({
+    width: 350, height: 300, image_format: 'png', png_quality: 90
+});
 
-}
+Camera = document.getElementById("camera");
 
-function setup() {
-    canvas = createCanvas(640, 400);
-    canvas.position(110, 250);
-
-    video = createCapture(VIDEO);
-    video.hide();
-
-    tint_color = "";
-}
-
-function draw() {
-    image(video, 0, 0, 600, 400);
-    tint(tint_color);
-}
-
-function filter_tint() {
-    tint_color = document.getElementById("color_name").value;
-}
+Webcam.attach('#camera');
 
 function take_snapshot() {
-    save('student_name.png');
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="capture_image" src="'+data_uri+'">'
+    })
+}
+
+console.log('ml5 version', ml5.version);
+
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/upk5z078A/model.json", modelLoaded);
+
+function modelLoaded() {
+    console.log("model loaded")
+}
+
+function check() {
+    img = document.getElementById("capture_image");
+
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if(error) {
+        console.error(error);
+    }
+    else {
+        console.log(results);
+
+        document.getElementById("result_emotion1").innerHTML = results[0].label;
+        gesture = results[0].label;
+        toSpeak = "";
+
+        if(gesture == "OK") {
+            toSpeak = "This is looking like you are putting up the OK Gesture."
+            document.getElementById("update_emoji1").innerHTML = "&#128076"
+        }
+        else if(gesture == "Peace") {
+            toSpeak = "This is looking like you are showing the Peace Symbol."
+            document.getElementById("update_emoji1").innerHTML = "&#9996"
+        }
+        else if(gesture == "Crossed Fingers") {
+            toSpeak = "This is looking like you are Crossing your Fingers."
+            document.getElementById("update_emoji1").innerHTML = "&#129310"
+        }
+        else if(gesture == "Pointing Up") {
+            toSpeak = "This is looking like you're Pointing Up."
+            document.getElementById("update_emoji1").innerHTML = "&#9757"
+        }
+        else if(gesture == "Thumbs Up/Down") {
+            toSpeak = "This is looking like you're showing a thumbs up or down."
+            document.getElementById("update_emoji1").innerHTML = "&#128078"
+        }
+        else if(gesture == "Waves") {
+            toSpeak = "This is looking like you're Waving."
+            document.getElementById("update_emoji1").innerHTML = "&#9995"
+        }
+        speak();
+    }
+}
+
+function speak() {
+    var synth = window.speechSynthesis;
+    speak_data = toSpeak;
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
 }
